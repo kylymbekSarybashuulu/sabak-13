2ae8c2dd22c41a69f53ac14caff3f2a71fd78da4
KYLYMBEK <kylymbek344@gmail.com>
2022-07-08 19:12:27 +0300

    commit papugay

---
+++ src/Papugay.java
@@ -0,0 +1,43 @@
+public class Papugay {
+
+
+    private String name;
+
+    private String breed;
+
+    private String color;
+
+    private String skill;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
+}
