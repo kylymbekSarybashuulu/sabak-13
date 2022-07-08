8ace651d61737846aa0747a3f09614d5a80d36f7
KYLYMBEK <kylymbek344@gmail.com>
2022-07-08 19:04:53 +0300

    commit fish

---
+++ src/Fish.java
@@ -0,0 +1,51 @@
+public class Fish {
+    private String name;
+    private String kingdom;
+    private String type;
+    private String classF;
+    private String color;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getKingdom() {
+        return kingdom;
+    }
+
+    public void setKingdom(String kingdom) {
+        this.kingdom = kingdom;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getClassF() {
+        return classF;
+    }
+
+    public void setClassF(String classF) {
+        this.classF = classF;
+    }
+
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+
+    }
+}
+
